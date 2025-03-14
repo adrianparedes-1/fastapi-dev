983b39b4988a3022eb4cef1933772c11a1ad327d
Adrian Paredes <adrianparedesroman99@gmail.com>
2025-03-14 15:00:03 -0400

    updated crud operations with sqlalchemy

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
+from multiprocessing import synchronize
 from fastapi import FastAPI, HTTPException, status, Response, Depends
-from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -15,12 +15,6 @@ app = FastAPI()
 
 my_posts = [{"title": "random", "content": "also random", "id": 1}, 
             {"title": "second", "content":"second stuff", "id": 2}]
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    id: int | None = None
 
 while True:
     try:
@@ -51,29 +45,52 @@ def root():
     return {"message": "Hello World"}
 
 @app.get("/posts")
-def posts():
-    cursor.execute("""SELECT * FROM posts """) #execute sql query
-    posts = cursor.fetchall() #get all the results from database and store them in a variable
-    return {"data": posts} #print the results
+def posts(db: Session = Depends(get_db)):
+    # Raw SQL query
+    # cursor.execute("""SELECT * FROM posts """) #execute sql query
+    # posts = cursor.fetchall() #get all the results from database and store them in a variable
+    
+    # SQLAlchemy query
+    posts = db.query(models.Post).all()
+    
+    return posts
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
-    # We are inserting using a parameterized query method to prevent SQL injections. This way any input is never executed as a SQL query but treated as a string
-    new_post = cursor.fetchone()
-    #there are staged changes but we need to commit it to the database to finalize them. Very similar to github repos
-    conn.commit()
-    return {"updated_post": new_post}
+def create_post(post: schemas.Post, db: Session = Depends(get_db)):
+    
+    # Raw SQL query
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
+    # # We are inserting using a parameterized query method to prevent SQL injections. This way any input is never executed as a SQL query but treated as a string
+    # new_post = cursor.fetchone()
+    # #there are staged changes but we need to commit it to the database to finalize them. Very similar to github repos
+    # conn.commit()
+    
+
+    # SQLAlchemy query
+    new_post = models.Post(**post.model_dump())
+    # print(new_post)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    
+    return new_post
 
 
 @app.get("/posts/{id}")
-def get_post(id: int): #anytime we have a path parameter, it will also be passed as a string so we need to parse it into an int for this to work
-    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (id,)) #we have to change it to tuple to let python know this is indexable (not super important) and tuples require a comma at the end if there is only one item
-    post = cursor.fetchone()
+def get_post(id: int, db: Session = Depends(get_db)): #anytime we have a path parameter, it will also be passed as a string so we need to parse it into an int for this to work
     
-    if not post:
+    #Raw SQL query
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (id,)) #we have to change it to tuple to let python know this is indexable (not super important) and tuples require a comma at the end if there is only one item
+    # post = cursor.fetchone()
+    
+    #SQLAlchemy query
+    new_post = db.query(models.Post).filter(id == models.Post.id).first()
+    
+    if not new_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    return {"post_detail": post}
+    
+    
+    return new_post
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
@@ -82,19 +99,27 @@ def test_posts(db: Session = Depends(get_db)):
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-
-    cursor.execute("""DELETE FROM posts * WHERE id = %s RETURNING *""", (id,))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-
-    if deleted_post == None: #if the deleted post was not found, then throw an error
+def delete_post(id: int, db: Session = Depends(get_db)):
+    #Raw SQL query
+    
+    # cursor.execute("""DELETE FROM posts * WHERE id = %s RETURNING *""", (id,))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    
+    #SQLAlchemy query
+    deleted_post = db.query(models.Post).filter(id == models.Post.id) #start db instance and query db based on id
+    print(deleted_post)
+    if deleted_post.first() == None: #if the deleted post was not found, then throw an error
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+
+    deleted_post.delete(synchronize_session=False) #delete() function removes the post found
+    db.commit() # commit changes
+    
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
-def update_post (id: int, post: Post):
+def update_post (id: int, post: schemas.Post):
     
     cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", 
                     (post.title, post.content, post.published, id,))
---
+++ app/schemas.py
@@ -0,0 +1,8 @@
+from . import models
+from pydantic import BaseModel
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    id: int | None = None
